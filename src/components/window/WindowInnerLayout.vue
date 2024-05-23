851ba64fa08a3bfa21fd1bfe21fd9fc25f01ef21
HJY <hwangjae1139@gmail.com>
2024-05-23 14:35:20 +0900

    feat: 윈도우 컴포넌트 구조 생성

--- src/components/window/WindowInnerLayout.vue
+++ src/components/window/WindowInnerLayout.vue
@@ -1,9 +1,26 @@
 <template>
-  <div class="window-inner">{{ currentId }}</div>
+  <div class="window-inner">
+    <WebPortfolio v-if="currentId === 'portfolio'" />
+    <MyInfo v-if="currentId === 'folder_user'" />
+    <FinderInner v-if="currentId === 'finder'" />
+    <LaunchPadInner v-if="currentId === 'launchpad'" />
+    <SettingsInner v-if="currentId === 'setting'" />
+    <TerminalInner v-if="currentId === 'terminal'" />
+    <FolderInner v-if="currentId === 'folder'" />
+    <TrashInner v-if="currentId === 'trash'" />
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { defineProps, computed } from "vue";
+import FinderInner from "@/components/window/contents/FinderInner.vue";
+import LaunchPadInner from "@/components/window/contents/LaunchPadInner.vue";
+import SettingsInner from "@/components/window/contents/SettingsInner.vue";
+import TerminalInner from "@/components/window/contents/TerminalInner.vue";
+import FolderInner from "@/components/window/contents/FolderInner.vue";
+import WebPortfolio from "@/components/window/contents/WebPortfolio.vue";
+import MyInfo from "@/components/window/contents/MyInfo.vue";
+import TrashInner from "@/components/window/contents/TrashInner.vue";
 const props = defineProps({
   windowId: {
     type: String,
@@ -19,5 +36,18 @@ const currentId = computed(() => props.windowId);
   border-radius: 14px;
   height: calc(100% - 40px);
   background: var(--window-inner-color);
+  overflow: auto;
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+  &::-webkit-scrollbar-thumb {
+    cursor: pointer;
+    height: 30%;
+    background: #d4d5d8;
+    border-radius: 2px;
+  }
+  &::-webkit-scrollbar-track {
+    background: rgba(91, 91, 91, 0.1);
+  }
 }
 </style>
---
+++ src/components/window/contents/SettingsInner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="inner-wrap">세팅</div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, computed } from "vue";
+</script>
+<style lang="scss" scoped>
+.inner-wrap {
+  width: 100%;
+  height: 100%;
+}
+</style>
